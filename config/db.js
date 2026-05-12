08bdb5b436482776ec8b90e6d543da7fa83eb626
Samarth MAlhotra <samarthmalhotra4200@gmail.com>
2026-05-12 10:46:59 +0530

    source code update

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 connection.connect((err) => {
   if (err) {
